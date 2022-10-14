f3952149cfcec69ac499c701564779185770d3ed
Menglin Wang <mw976@cornell.edu>
2022-10-14 02:30:12 -0400

    basic layout

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request
 from flask import render_template
-app= Flask(__name__)
-app.config['DEBUG'] = True
+app = Flask(__name__)
+app.config["DEBUG"] = True
 app.config["APPLICATION_ROOT"] = "/"
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/', methods=["GET", "POST"])
-
 def main():
     return render_template('index.html')
