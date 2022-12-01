94c3ce0827a234fdad6f5458cf227acf2e620052
Menglin Wang <mw976@cornell.edu>
2022-12-01 16:28:54 -0500

    update

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import json
 import os
 import collections
 import overpy
+import numpy as np
 app = Flask(__name__)
 app.config["DEBUG"] = True
 app.config["APPLICATION_ROOT"] = "/"
@@ -89,7 +90,7 @@ def fetch_data(period):
         item_list.append(item)
     return item_list
 
-def generate_markers(most_concern, second_concern, item_list):
+def generate_concern_markers(most_concern, second_concern, item_list):
     counter= collections.Counter(item['val'][2])
     most_concern = counter.most_common(1)[0][0]
     second_concern = counter.most_common(2)[1][0] 
@@ -119,7 +120,60 @@ def generate_markers(most_concern, second_concern, item_list):
                 {idd}.addTo(map).bindPopup('{cat}');".format(idd="idd"+str(idd), latitude=lat,\
                                                                             longitude=lon, cat=cat)
     return most_concern_marker, second_concern_marker, most_concern_list, second_concern_list
-def get_suggestions(most_concern_list, second_concern_list):
+
+def generate_infra_markers(shops, restaurants, hotels, hospitals):
+    shop_marker=''
+    restaurant_marker=''
+    hotel_marker=''
+    hospital_marker=''
+    idd= 0
+    for shop in shops.nodes:
+        idd+=1
+        lon = shop.lon
+        lat = shop.lat
+        try:
+            shop_brand = shop.tags['brand']
+        except:
+            shop_brand = 'null'
+        shop_marker += "var {idd} = L.marker([{latitude}, {longitude}]);\
+            {idd}.addTo(map).bindPopup('shop<br> {brand}');".format(idd="idd"+str(idd), latitude=lat,\
+                                                                        longitude=lon, brand=shop_brand)
+    for restaurant in restaurants.nodes:
+        idd+=1
+        lon = restaurant.lon
+        lat = restaurant.lat
+        try:
+            shop_brand = restaurant.tags['brand']
+        except:
+            shop_brand = 'null'
+        restaurant_marker += "var {idd} = L.marker([{latitude}, {longitude}]);\
+            {idd}.addTo(map).bindPopup('restaurant<br> {brand}');".format(idd="idd"+str(idd), latitude=lat,\
+                                                                        longitude=lon, brand=shop_brand)
+    for hotel in hotels.nodes:
+        idd+=1
+        lon = hotel.lon
+        lat = hotel.lat
+        try:
+            shop_brand = hotel.tags['brand']
+        except:
+            shop_brand = 'null'
+        hotel_marker += "var {idd} = L.marker([{latitude}, {longitude}]);\
+            {idd}.addTo(map).bindPopup('hotel <br> {brand}');".format(idd="idd"+str(idd), latitude=lat,\
+                                                                        longitude=lon, brand=shop_brand)
+    for hospital in hospitals.nodes:
+        idd+=1
+        lon = hospital.lon
+        lat = hospital.lat
+        try:
+            shop_brand = hospital.tags['brand']
+        except:
+            shop_brand = 'null'
+        hospital_marker += "var {idd} = L.marker([{latitude}, {longitude}]);\
+            {idd}.addTo(map).bindPopup('hospital <br> {shop_brand}');".format(idd="idd"+str(idd), latitude=lat,\
+                                                                        longitude=lon, shop_brand=shop_brand)
+    return shop_marker, restaurant_marker, hotel_marker, hospital_marker
+
+def get_zip_count(most_concern_list, second_concern_list):
     # provide the suggestions for the user
     area_list = read_area()['zipcode']
     zipcode_map={}
@@ -137,14 +191,14 @@ def get_suggestions(most_concern_list, second_concern_list):
         lon = item['val'][0]
         lat = item['val'][1]    
         zipcode = get_zipcode(lon, lat)
-        tmp= []([0], [0] ,[0])
+        tmp= []([0], [0] ,[])
         zip_count[zipcode]= zip_count.get(zipcode, tmp)
         zip_count[zipcode][0][0]+=1
     for item in second_concern_list:
         lon = item['val'][0]
         lat = item['val'][1]    
         zipcode = get_zipcode(lon, lat)
-        tmp= [](0, 0 ,0)
+        tmp= []([0], [0] ,[])
         zip_count[zipcode]= zip_count.get(zipcode, tmp)
         zip_count[zipcode][1][0]+=1
     zip_list = list(zip_count.keys())
@@ -156,14 +210,43 @@ def get_suggestions(most_concern_list, second_concern_list):
     for zip in zip_list:
         info= zipcode_map[zip] 
         shops, restaurants, hotels, hospitals = get_infrastructure(info[1], info[0], info[2])
+        shop_marker, restaurant_marker, hotel_marker, hospital_marker = generate_infra_markers(shops, restaurants, hotels, hospitals)
         zip_count[zip][2]= [len(shops.nodes), len(restaurants.nodes), len(hotels.nodes), len(hospitals.nodes), 
-            len(shops.nodes)+len(restaurants.nodes)+len(hotels.nodes)+len(hospitals.nodes)]
+            len(shops.nodes)+len(restaurants.nodes)+len(hotels.nodes)+len(hospitals.nodes), shop_marker, restaurant_marker, hotel_marker, hospital_marker]
     
-    # sort the zip_count by the number of infrastructure and hate_factor:Num of most concern * 0.7 + second concern * 0.3. and return the top 3
-    zip_count = sorted(zip_count.items(), key=lambda x: x[1][2][4]*0.3+x[1][0][0]*0.7+x[1][1][0]*0.3, reverse=True)
+    return zip_count
+
+def get_suggestion(zip_count):
+    zip_list= list(zip_count.keys())
+    total_infra= []
+    mconcern= []
+    sconcern= []
+    # normalize the total number of infrastructure for each area
+    for zip in zip_list:
+        total_infra.append(zip_count[zip][2][4])
+        mconcern.append(zip_count[zip][0][0])
+        sconcern.append(zip_count[zip][1][0])
+    total_infra= np.array(total_infra)
+    sum= np.sum(total_infra)
+    total_infra=[total_infra[i]/sum for i in range(len(total_infra))]
+    for zip in zip_list:
+        zip_count[zip][2][4]= total_infra[zip_list.index(zip)]
+    # normalize the number of most concern and second concern for each area
+    mconcern= np.array(mconcern)
+    sconcern= np.array(sconcern)
+    sum= np.sum(mconcern)
+    sum= np.sum(sconcern)
+    mconcern=[mconcern[i]/sum for i in range(len(mconcern))]
+    sconcern=[sconcern[i]/sum for i in range(len(sconcern))]
+    for zip in zip_list:
+        zip_count[zip][0][0]= mconcern[zip_list.index(zip)]
+        zip_count[zip][1][0]= sconcern[zip_list.index(zip)]
+
+    # sort the zip_count by the normalized number of infrastructure and normalized hate_factor(# of most concern * 0.7 + second concern * 0.3. and return the top 3)
+    zip_count = sorted(zip_count.items(), key=lambda x: x[1][2][4]*0.2-x[1][0][0]*0.4-x[1][1][0]*0.2, reverse=True)
     recommendation = []
     for i in range(3):
-        recommendation.append(zip_count[i][0])  
+        recommendation.append(zip_count[i])  
     return recommendation
    
 
